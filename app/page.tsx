6152267fb6d8bb266956e4b37ce97812103b9322
maritocuate <mario300@gmail.com>
2023-05-09 17:32:15 -0300

    add: getPosts action

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,38 @@
+import Container from "./components/Container"
+import getPosts, { 
+  IPostsParams
+} from './actions/getPosts'
 
-export default function Home() {
+interface HomeProps {
+  searchParams: IPostsParams
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const posts = await getPosts(searchParams)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Main
-    </main>
+    <Container>
+      <div
+        className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {
+          posts.map((post:any) => (
+            <div key={post.id}>
+              {post.title}
+            </div>
+          ))
+        }
+      </div>
+    </Container>
   )
 }
