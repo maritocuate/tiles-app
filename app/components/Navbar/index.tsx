c126e0a306765f8738cbbe2c7fef0f2e311e94b8
maritocuate <mario300@gmail.com>
2023-05-16 00:24:53 -0300

    chore: Navbar component styles update

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -25,10 +25,10 @@ const Navbar: React.FC<NavbarProps> = ({
         <div className={`fixed w-full z-10 shadow-sm ${styles.container}`}>
             <div className="py-4">
                 <Container>
-                    <div className="grid grid-cols-3 gap-3 md:gap-0">
+                    <div className="grid grid-cols-4">
                         <div className={styles.title}>Tiles</div>
                         
-                        <Tabs value={1}>
+                        <Tabs value={1} className="col-start-2 col-span-2" >
                             <TabsHeader>
                                 <Tab className='px-8' value={1}>
                                     <AiFillFire size={20} />
